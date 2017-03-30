bf3d142551484046dd31d97c3e4fa9d6d81876e0
Hue  Vang <huevang@Hues-MacBook-Pro.local>
2017-03-30 16:27:06 -0500

    Added process.env.PORT

--- server.js
+++ server.js
@@ -71,6 +71,6 @@ app.get('/*', function(req, res){
 
 
 
-var server = app.listen(3000, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
   console.log('Listening on port', server.address().port);
 });
