3b998f3e82f45bab53568be3d418f234a84ef4fd
Hue  Vang <huevang@Hues-MacBook-Pro.local>
2017-03-31 12:16:41 -0500

    Added more config settings

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,8 @@
 var pg = require('pg');
 var pool = new pg.Pool({
+  user: 'idwqxqspnhwibt', //env var: PGUSER
   database: 'dljn2v22d1htu', //env var: PGDATABASE
-  // password: 'secret', //env var: PGPASSWORD
+  password: '1e1434b18a9ee44d5a4bdc3ef1d9981cf78c65efcf70f4cf5f23c07e5876036d', //env var: PGPASSWORD
   host: 'ec2-54-235-92-236.compute-1.amazonaws.com', // Server hosting the postgres database
   port: 5432 //env var: PGPORT
 });
--- server.js
+++ server.js
@@ -5,9 +5,9 @@ var session = require('express-session');
 var passport = require('passport');
 // var config = {database : DATABASE_URL};
 var config = {
-  // user: 'foo', //env var: PGUSER
+  user: 'idwqxqspnhwibt', //env var: PGUSER
   database: 'dljn2v22d1htu', //env var: PGDATABASE
-  // password: 'secret', //env var: PGPASSWORD
+  password: '1e1434b18a9ee44d5a4bdc3ef1d9981cf78c65efcf70f4cf5f23c07e5876036d', //env var: PGPASSWORD
   host: 'ec2-54-235-92-236.compute-1.amazonaws.com', // Server hosting the postgres database
   port: 5432 //env var: PGPORT
   // max: 10, // max number of clients in the pool
