3199616799d2173e133cdbb0a712d232b71a0dff
Hue  Vang <huevang@Hues-MacBook-Pro.local>
2017-03-31 01:23:16 -0500

    ok

--- server.js
+++ server.js
@@ -71,6 +71,6 @@ app.get('/*', function(req, res){
 
 
 
-var server = app.listen(process.env.PORT, function() {
+var server = app.listen(process.env.PORT || 3000, function() {
   console.log('Listening on port', server.address().port);
 });
